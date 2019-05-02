d2c89ac8aa021d04c52249bc30338cf131c5a4de
Indra Setyajiwirawan Guritno <indrasetyajiwirawanguritno@gmail.com>
2019-05-02 22:47:47 +0700

    add route coverage

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ const topicRoutes = require('./routes/topic');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('/coverage', express.static('coverage'));
 app.use('/', topicRoutes);
 
 const PORT = 8888;
-app.listen(PORT, () => { console.log(`App listening on port ${PORT}`); });
+app.listen(process.env.PORT || PORT, () => { console.log(`App listening on port ${PORT}`); });
 
 module.exports = app;
